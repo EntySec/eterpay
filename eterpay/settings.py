f580c57e528928b1715c2d2a822e4004767d0474
Ivan Nikolsky <enty8080@gmail.com>
2023-10-22 22:46:34 +0100

    Update settings.py

--- eterpay/settings.py
+++ eterpay/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
